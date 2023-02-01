5a91215e2c2ae3cb870eccdf57a0069106dcb019
jacksonlevine <jack@netbug.us>
2023-02-01 14:35:10 -0800

    test passed!

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -7,7 +7,6 @@ describe ('Player object functionality', () => {
     newPlayer = new Player('Test Name');
   })
 
-  
 
   test('it should create a player object with a custom name that you give it, and health of 500, and an empty inventory array', () => {
     expect(newPlayer).toEqual({name:"Test Name", health:500, inventory:[]})
