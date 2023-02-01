ea17163ace2bb265da139ce03b29856571093227
jacksonlevine <jack@netbug.us>
2023-02-01 14:21:23 -0800

    Co-authored-by: Owen <osbrown1@users.noreply.github.com> Co-authored-by: Carl Sustrich <CarlSustrich@users.noreply.github.com>

---
+++ __tests__/game.test.js
@@ -0,0 +1,17 @@
+import { Player } from './../src/game.js';
+
+describe ('Player object functionality', () => {
+  let newPlayer;
+
+  beforeEach(() => {
+    newPlayer = new Player('Test Name');
+  })
+
+  
+
+  test('it should create a player object with a custom name that you give it, and health of 500, and an empty inventory array', () => {
+    expect(newPlayer).toEqual({name:"Test Name", health:500, inventory:[]})
+  });
+
+
+})
